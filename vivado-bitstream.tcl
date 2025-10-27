8c342d01b24c194f0e9643422dc20574059590c8
liyuxuan <liyuxuan3003@outlook.com>
2025-10-27 11:49:27 +0800

    添加注释

--- vivado-bitstream.tcl
+++ vivado-bitstream.tcl
@@ -1,17 +1,41 @@
+#### Generate Vivado Bitstream ####
+
+# Arguments: $srcs_verilog $srcs_xdc $module_top $platform $bitstream
+
+# Sources of verilog
 set srcs_verilog [lindex $argv 0]
+
+# Sources of xdc
 set srcs_xdc [lindex $argv 1]
+
+# Top level module name
 set module_top [lindex $argv 2]
+
+# Target platform
 set platform [lindex $argv 3]
+
+# Bitstream file (.bit)
+# We will also create .bin file (for burn) which share the same name with .bit file (for load)
 set bitstream [lindex $argv 4]
 
+# Read verilog
 read_verilog $srcs_verilog
+# Read xdc
 read_xdc $srcs_xdc
 
+# Synthesis design
 synth_design -top $module_top -part $platform
+
+# Optimize design
 opt_design
+
+# Place design
 place_design
+
+# Route design
 route_design
 
+# Generate .bit and .bin file
 write_bitstream -bin_file -force $bitstream
 
 exit
--- vivado-program.tcl
+++ vivado-program.tcl
@@ -1,70 +1,64 @@
+#### Program Vivado FPGA ####
+
+# Arguments: $bitstream $flash $mode
+
+# Bitstream file
+# .bit for mode == load
+# .bin for mode == burn
 set bitstream [lindex $argv 0]
+
+# Flash id
 set flash [lindex $argv 1]
+
+# Program mode: load / burn
+# load -> program on jtag
+# burn -> program on flash
 set mode [lindex $argv 2]
 
+# Connect hardware
 open_hw_manager
 connect_hw_server
 open_hw_target
 
-
-# # set devPart [get_property PART [current_hw_device]]
-# # set cfgParts [get_cfgmem_parts -of [get_parts $devPart]]
-# create_hw_cfgmem -hw_device [current_hw_device] $flash
-# set cfgMem [current_hw_cfgmem]
-# set_property PROGRAM.FILE $bitstream $cfgMem
-# set_property PROGRAM.ADDRESS_RANGE  {use_file} $cfgMem
-# set_property PROGRAM.BLANK_CHECK  1 $cfgMem
-# set_property PROGRAM.ERASE  1 $cfgMem
-# set_property PROGRAM.CFG_PROGRAM  1 $cfgMem
-# set_property PROGRAM.VERIFY  1 $cfgMem
-# program_hw_cfgmem $cfgMem
-
-
-
-
+# Get hardware
 set hw_device [lindex [get_hw_devices] 0]
 
+# Get flash
 set mem_device [lindex [get_cfgmem_parts $flash] 0]
 create_hw_cfgmem -hw_device $hw_device $mem_device
-
 set hw_cfgmem [get_property PROGRAM.HW_CFGMEM $hw_device]
 
+# If mode == load
 if {[string equal $mode "load"]} {
+    # Set bitstream path
     set_property PROGRAM.FILE $bitstream $hw_device
+    # Program device (jtag)
     program_hw_device $hw_device
 }
 
+# If mode == burn
 if {[string equal $mode "burn"]} {
+    # Set some property
     set_property PROGRAM.BLANK_CHECK 0 $hw_cfgmem
     set_property PROGRAM.ERASE 1 $hw_cfgmem
     set_property PROGRAM.CFG_PROGRAM 1 $hw_cfgmem
     set_property PROGRAM.VERIFY 1 $hw_cfgmem
     set_property PROGRAM.CHECKSUM 0 $hw_cfgmem
-    set_property PROGRAM.FILES $bitstream $hw_cfgmem
     set_property PROGRAM.ADDRESS_RANGE {use_file} $hw_cfgmem
-    # create_hw_bitstream -hw_device [lindex [get_hw_devices xc7a35t_0] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices xc7a35t_0] 0]]; program_hw_devices [lindex [get_hw_devices xc7a35t_0] 0]; refresh_hw_device [lindex [get_hw_devices xc7a35t_0] 0];
-    # program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+    # Set bitstream path
+    set_property PROGRAM.FILES $bitstream $hw_cfgmem
+    # Create bitstream object
     set hw_cfgmem_bitfile [get_property PROGRAM.HW_CFGMEM_BITFILE $hw_device]
     create_hw_bitstream -hw_device $hw_device $hw_cfgmem_bitfile
+    # We don't really understand these two commands, seems to do jtag program and then refresh?
+    # But it is necessary before flash program, or it will cause error.
     program_hw_device $hw_device
     refresh_hw_device $hw_device
+    # Program device (flash)
     program_hw_cfgmem -hw_cfgmem $hw_cfgmem
 }
 
-# create_hw_cfgmem -hw_device [get_hw_devices xc7a35t_0] -mem_dev [lindex [get_cfgmem_parts {n25q64-3.3v-spi-x1_x2_x4}] 0]
-# set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-# set_property PROGRAM.FILES [list "/home/workspace/Course/20250915-EE291F/EE291FTest/EE291FTestVivado/EE291FTest.runs/impl_1/demoLab2.bin" ] [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-# set_property PROGRAM.PRM_FILE {} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-# set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-# set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-# set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-# set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-# set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-# set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-# startgroup 
-# create_hw_bitstream -hw_device [lindex [get_hw_devices xc7a35t_0] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices xc7a35t_0] 0]]; program_hw_devices [lindex [get_hw_devices xc7a35t_0] 0]; refresh_hw_device [lindex [get_hw_devices xc7a35t_0] 0];
-
-
+# Disconnect hardware
 close_hw_target
 disconnect_hw_server
 close_hw_manager
